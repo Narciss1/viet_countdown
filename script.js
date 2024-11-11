c00d3dbb5edb6320f8a5fbb2865b5046f6282bf4
Lina Sadgal <lina.sadgal@epfl.ch>
2024-11-11 18:29:33 +0100

    Ajout du site de compte à rebours

---
+++ script.js
@@ -0,0 +1,23 @@
+function updateCountdown() {
+    const countdownDate = new Date("February 2, 2025 00:00:00").getTime();
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+
+    if (distance < 0) {
+        clearInterval(countdownInterval);
+        document.getElementById("countdown").innerHTML = "Let's goooo Vietnaaam !";
+    }
+}
+
+const countdownInterval = setInterval(updateCountdown, 1000);
+updateCountdown();
